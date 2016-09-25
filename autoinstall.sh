14e4a14926f94c4cb5b8a9d71aeb5938155e366f
Lentin <codexss@users.noreply.github.com>
2016-09-25 13:34:28 +0800

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-NGINX_VERSION='1.11.1'
-PCRE_VERSION='8.38'
+NGINX_VERSION='1.11.4'
+PCRE_VERSION='8.39'
+OPENSSL_VERSION='1.1.0a'
+ZLIB_VERSION='1.2.8'
 
 cd $OPENSHIFT_TMP_DIR
 
 wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar xzf nginx-${NGINX_VERSION}.tar.gz
-wget http://exim.mirror.fr/pcre/pcre-${PCRE_VERSION}.tar.gz
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.gz
 tar xzf pcre-${PCRE_VERSION}.tar.gz
-git clone https://github.com/FRiCKLE/ngx_cache_purge.git
-git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git
+wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
+tar xzf openssl${OPENSSL_VERSION}.tar.gz
+wget http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
+tar xzf zlib-${ZLIB_VERSION}.tar.gz
+git clone https://github.com/cuber/ngx_http_google_filter_module
+git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
 cd ${OPENSHIFT_TMP_DIR}nginx-${NGINX_VERSION}
-./configure --prefix=$OPENSHIFT_DATA_DIR --with-pcre=${OPENSHIFT_TMP_DIR}pcre-${PCRE_VERSION} --with-pcre-jit --with-threads --with-http_realip_module --with-http_sub_module --with-http_stub_status_module --with-http_ssl_module --with-http_gzip_static_module --with-http_auth_request_module --with-http_geoip_module --with-http_secure_link_module --without-mail_pop3_module
---without-mail_imap_module --without-mail_smtp_module --add-module=${OPENSHIFT_TMP_DIR}ngx_http_substitutions_filter_module --add-module=${OPENSHIFT_TMP_DIR}ngx_cache_purge
+./configure \
+  --prefix=$OPENSHIFT_DATA_DIR \
+  --with-pcre=../pcre-${PCRE_VERSION} \
+  --with-openssl=../openssl-${OPENSSL_VERSION} \
+  --with-zlib=../zlib-${ZLIB_VERSION} \
+  --with-http_ssl_module \
+  --add-module=../ngx_http_google_filter_module \
+  --add-module=../ngx_http_substitutions_filter_module
 make -j4 && make install
-cd /tmp
-rm -rf *
+rm -rf ${OPENSHIFT_TMP_DIR}
 cd ${OPENSHIFT_REPO_DIR}.openshift/action_hooks
 rm -rf start
 cat>start<<EOF
@@ -25,62 +36,28 @@ cat>start<<EOF
 # script. The application will work only if it binds to
 # \$OPENSHIFT_DIY_IP:8080
 #nohup \$OPENSHIFT_REPO_DIR/diy/testrubyserver.rb \$OPENSHIFT_DIY_IP \$OPENSHIFT_REPO_DIR/diy |& /usr/bin/logshifter -tag diy &
-nohup \$OPENSHIFT_DATA_DIR/sbin/nginx > \$OPENSHIFT_LOG_DIR/server.log 2>&1 &
+sed -e "s/`echo '$OPENSHIFT_IP:$OPENSHIFT_PORT'`/`echo $OPENSHIFT_DIY_IP:$OPENSHIFT_DIY_PORT`/;s/8.8.8.8/`cat /etc/resolv.conf |grep -i nameserver|head -n1|cut -d ' ' -f2`/" $OPENSHIFT_DATA_DIR/conf/nginx.conf.template > $OPENSHIFT_DATA_DIR/conf/nginx.conf
+nohup $OPENSHIFT_DATA_DIR/sbin/nginx > $OPENSHIFT_DIY_LOG_DIR/server.log 2>&1 &
 EOF
 chmod 755 start
-cd ${OPENSHIFT_REPO_DIR}.openshift/cron/minutely
-rm -rf restart.sh
-cat>restart.sh<<EOF
+rm -rf stop
+cat>stop<<EOF
 #!/bin/bash
-export TZ='Asia/Shanghai'
+source $OPENSHIFT_CARTRIDGE_SDK_BASH
 
-curl -I \${OPENSHIFT_APP_DNS} 2> /dev/null | head -1 | grep -q '200\|301\|302\|404\|403'
-
-s=\$?
-
-if [ \$s != 0 ];
-	then
-		echo "`date +"%Y-%m-%d %H:%M:%S"` down" >> \${OPENSHIFT_LOG_DIR}web_error.log
-		echo "`date +"%Y-%m-%d %H:%M:%S"` restarting..." >> \${OPENSHIFT_LOG_DIR}web_error.log
-		killall nginx
-		nohup \${OPENSHIFT_DATA_DIR}/sbin/nginx > \${OPENSHIFT_LOG_DIR}/server.log 2>&1 &
-		#/usr/bin/gear start 2>&1 /dev/null
-		echo "`date +"%Y-%m-%d %H:%M:%S"` restarted!!!" >> \${OPENSHIFT_LOG_DIR}web_error.log		
-else
-	echo "`date +"%Y-%m-%d %H:%M:%S"` is ok" > \${OPENSHIFT_LOG_DIR}web_run.log
-fi
-EOF
-chmod 755 restart.sh
-touch nohup.out
-chmod 755 nohup.out
-rm -rf delete_log.sh
-cat>delete_log.sh<<EOF
-#!/bin/bash
-export TZ="Asia/Shanghai"
-
-# 每天 00:30 06:30 12:30 18:30 删除一次网站日志
-hour="`date +%H%M`"
-if [ "\$hour" = "0030" -o "\$hour" = "0630" -o "\$hour" = "1230" -o "\$hour" = "1830" ]
+# The logic to stop your application should be put in this script.
+if [ -z "$(ps -ef | grep nginx | grep -v grep)" ]
 then
-  echo "Scheduled delete at \$(date) ..." >&2
-  (
-  sleep 1
-  cd \${OPENSHIFT_LOG_DIR}
-  rm -rf *
-  echo "delete OPENSHIFT_LOG at \$(date) ..." >&2
-  sleep 1
-  cd \${OPENSHIFT_DATA_DIR}/logs
-  rm -rf *.log
-  echo "delete nginx logs at \$(date) ..." >&2
-  ) &
-  exit
+    client_result "Application is already stopped"
+else
+    kill `ps -ef | grep nginx | grep -v grep | awk '{ print $2 }'` > /dev/null 2>&1
 fi
 EOF
-chmod 755 delete_log.sh
+chmod 755 stop
 
 cd $OPENSHIFT_DATA_DIR/conf
 rm nginx.conf
-wget --no-check-certificate https://github.com/tcpit/openshift-nginx-proxy/raw/master/nginx.conf
+wget --no-check-certificate https://github.com/codexss/openshift-nginx-proxy/raw/master/nginx.conf
 sed -i "s/OPENSHIFT_DIY_IP/$OPENSHIFT_DIY_IP/g" nginx.conf
 sed -i "s/xxx-xxx.rhcloud.com/$OPENSHIFT_APP_DNS/g" nginx.conf
 gear stop
